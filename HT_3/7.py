e85667606dfd7807f0c677e042caed08382d6ca9
pavlokucheriavyi <kuchriavy10@gmail.com>
2021-11-24 13:08:29 +0200

    update 7 task

--- HT_3/7.py
+++ HT_3/7.py
@@ -3,21 +3,28 @@
 повинна бути 1 ф-цiя яка б приймала 3 аргументи - один з яких операцiя, яку зробити!
 '''
 
-my_string = input("Enter two numbers and one of the operations: +, -, /, //, *, %. Example:12 % 3 ").split(" ")
 
-def my_function(num_1, operation, num_2):
-    if operation == '+':
-        return int(num_1) + int(num_2)
-    elif operation == '-':
-        return int(num_1) - int(num_2)
-    elif operation == '/':
-        return int(num_1) / int(num_2)
-    elif operation == '//':
-        return int(num_1) // int(num_2)
-    elif operation == '*':
-        return int(num_1) * int(num_2)
-    else:
-        return int(num_1) % int(num_2)
+def my_function():
+    some = True
+    while some == True:
+        try:
+            my_string = input("Enter two numbers and one of the operations: +, -, /, //, *, %. Example(12 % 3) ").split(" ")
+            if my_string[1] == '+':
+                return int(my_string[0]) + int(my_string[2])
+            elif my_string[1] == '-':
+                return int(my_string[0]) - int(my_string[2])
+            elif my_string[1] == '/':
+                return int(my_string[0]) / int(my_string[2])
+            elif my_string[1] == '//':
+                return int(my_string[0]) // int(my_string[2])
+            elif my_string[1] == '*':
+                return int(my_string[0]) * int(my_string[2])
+            else:
+                return int(my_string[0]) % int(my_string[2])
+            some = False
+        except ZeroDivisionError:
+            print("You cannot divide by zero, try again!!!")
 
-result = my_function(my_string[0], my_string[1], my_string[2])
+
+result = my_function()
 print(result)
