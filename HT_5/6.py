b5ce73e6dd01964e7a85e151bf3b341029962f79
pavlokucheriavyi <kuchriavy10@gmail.com>
2021-12-02 10:52:56 +0200

    update

--- HT_5/6.py
+++ HT_5/6.py
@@ -2,20 +2,55 @@
     Всі ви знаєте таку функцію як <range>. Напишіть свою реалізацію цієї функції.
     P.S. Повинен вертатись генератор.
 '''
+
+class NotEnoughError(Exception):
+    def __init__(self, text):
+        self.text = text
+
 class StepZeroWarningError(Exception):
-    pass
+    def __init__(self, text):
+        self.text = text
 
-def my_range(num_1, num_2, step=1):
-    if step == 0:
-        raise StepZeroWarningError(step)
+class TooManyElementsError(Exception):
+    def __init__(self, text):
+        self.text = text
 
-    my_list = [num_1]
-    while num_1 < num_2:
-        num_1 += step
-        my_list.append(num_1)
-    my_gener = (x for x in my_list)
-    return my_gener
+def my_range(*args):
+    try:
+        if len(args) == 3:
+            num_1 = args[0]
+            num_2 = args[1]
+            step = args[2]
+        elif len(args) == 2:
+            num_1 = args[0]
+            num_2 = args[1]
+            step = 1
+        elif len(args) == 1:
+            num_1 = 0
+            num_2 = args[0]
+            step = 1
+        elif len(args) == 0:
+            raise NotEnoughError("some")
+        else:
+            raise TooManyElementsError('some')
 
-list_of_range_function = my_range(1, 10, 3)
-print(next(list_of_range_function))
-print(list_of_range_function.__next__())
+
+        if step == 0:
+            raise StepZeroWarningError("some")
+
+
+        while num_1 < num_2:
+            yield num_1
+            num_1 += step
+
+    except TooManyElementsError as Tmee:
+        print("TooManyElementsError: too many elements for the 'range' function. Should not be more than 3")
+    except StepZeroWarningError as Szwe:
+        print("StepZeroWarningError: the step of the 'range' function cannot be zero")
+    except NotEnoughError as Nee:
+        print("NotEnoughError: the 'range' function must accept at least one element")
+
+
+list_of_range_function = my_range()
+for elem in list_of_range_function:
+    print(elem)
