28e825d53c2e6c4e66ca131ce93c1324c0c2257f
pavlokucheriavyi <kuchriavy10@gmail.com>
2021-12-25 18:12:39 +0200

    Home task 10 fertig

---
+++ HT_10/2.py
@@ -0,0 +1,100 @@
+import requests
+import json
+from datetime import timedelta, date, datetime
+
+
+
+flag = True
+while flag == True:
+    cur = input('Enter Currence (example: EUR, USD, RUB, GBP, PLN): ')
+
+    now = datetime.now()
+    current_datetime = now
+    date_format = '%d.%m.%Y'
+    m = current_datetime.strftime(date_format)
+
+    URL = 'https://api.privatbank.ua/p24api/exchange_rates?json&date=' + m
+    r = requests.get(URL)
+    str_json = r.text
+    data = json.loads(str_json)
+    my_list = []
+
+    for i in data['exchangeRate']:
+        for k, v in i.items():
+            if v == cur:
+                my_list.append(v)
+
+    if len(my_list) == 0:
+        print('This currency does not exist, please try again - (example: EUR, USD, RUB, GBP, PLN)')
+    else:
+        flag = False
+
+
+
+def check_date():
+    flag = True
+    while flag == True:
+        try:
+            date = input('Enter date "day"."monat"."year", (example: 2.4.2021): ')
+
+
+
+            date_format = "%d.%m.%Y"
+
+            start = datetime.strptime(date, date_format)
+            now = datetime.now()
+
+            list1 = date.split('.')
+
+            if start > now:
+                print('Your date in the future, please enter again')
+            else:
+                return int(list1[0]), int(list1[1]), int(list1[2])
+        except ValueError:
+            print('You entered the wrong date, perhaps this day is not on the calendar. Please repeat again. Example(12.11.2021)')
+
+
+something = check_date()
+year = something[2]
+month = something[1]
+day = something[0]
+
+
+
+def daterange(start_date, end_date):
+    for i in range(int((end_date - start_date).days) + 1):
+        yield start_date + timedelta(i)
+
+start_date = date(year, month, day)
+end_date = datetime.now().date()
+
+print('Currency:' + cur)
+some = 0
+
+for single_date in daterange(start_date, end_date):
+    m = single_date.strftime("%d.%m.%Y")
+    URL = 'https://api.privatbank.ua/p24api/exchange_rates?json&date=' + m
+    r = requests.get(URL)
+    str_json = r.text
+    data = json.loads(str_json)
+
+
+
+    for i in data['exchangeRate']:
+        for k, v in i.items():
+            if v == cur:
+                result_dif = '------'
+                if some == 0:
+                    some = i['purchaseRateNB']
+                    print('Date:', m)
+                    print('NBU:', i['purchaseRateNB'], '    ', result_dif)
+                elif some != 0 and some != i['purchaseRateNB']:
+                    result_dif = i['purchaseRateNB'] - some
+                    some = i['purchaseRateNB']
+                    print('Date:', m)
+                    print('NBU:', i['purchaseRateNB'], '    ', '%.4f' % result_dif)
+                elif some != 0 and some == i['purchaseRateNB']:
+                    result_dif = '------'
+                    some = i['purchaseRateNB']
+                    print('Date:', m)
+                    print('NBU:', i['purchaseRateNB'], '    ', result_dif)
---
+++ HT_10/3.py
@@ -0,0 +1,127 @@
+import requests
+import json
+from datetime import datetime
+
+
+def check_cur(main_input):
+    flag = True
+    while flag == True:
+        current_datetime = datetime.now()
+        date_format = '%d.%m.%Y'
+        m = current_datetime.strftime(date_format)
+
+        URL = 'https://api.privatbank.ua/p24api/exchange_rates?json&date=' + m
+        r = requests.get(URL)
+        str_json = r.text
+        data = json.loads(str_json)
+        my_list = []
+
+        for i in data['exchangeRate']:
+            for k, v in i.items():
+                if v == main_input:
+                    my_list.append(v)
+
+        if len(my_list) == 0:
+            print('This currency does not exist, please try again - (example: EUR, USD, RUB, GBP, PLN)')
+            return False
+        else:
+            return True
+
+
+flag = True
+while flag == True:
+    my_input1 = input('Enter first currency (example EUR, USD, RUB, GBP, PLN): ')
+    some = check_cur(my_input1)
+    if some == False:
+        continue
+    my_input2 = input('Enter second currency (example EUR, USD, RUB, GBP, PLN): ')
+    some2 = check_cur(my_input2)
+    if some2 == False:
+        continue
+    else:
+        flag = False
+    summ_for_sale = float(input('Enter summ of money (example: 1340): '))
+
+current_datetime = datetime.now()
+date_format = '%d.%m.%Y'
+m = current_datetime.strftime(date_format)
+
+URL = 'https://api.privatbank.ua/p24api/exchange_rates?json&date=' + m
+r = requests.get(URL)
+str_json = r.text
+data = json.loads(str_json)
+
+if len(data) == 0:
+    date_format = '%d.%m.%Y'
+    tomorrow = current_datetime - timedelta(days=1)
+    last_tomorrow = tomorrow.strftime(date_format)
+    URL = 'https://api.privatbank.ua/p24api/exchange_rates?json&date=' + last_tomorrow
+    r = requests.get(URL)
+    str_json = r.text
+    data = json.loads(str_json)
+
+
+    def konv(cur1, cur2, summ):
+        result_summ = 0
+
+        if cur1 == 'UAH':
+            for i in data['exchangeRate']:
+                for k, v in i.items():
+                    if v == my_input2:
+                        result_summ = summ / i['saleRate']
+                        return result_summ
+        elif cur2 == 'UAH':
+            for i in data['exchangeRate']:
+                for k, v in i.items():
+                    if v == my_input1:
+                        result_summ = summ * i['purchaseRate']
+                        return result_summ
+
+        for i in data['exchangeRate']:
+            for k, v in i.items():
+                if v == my_input1:
+                    result_summ = summ * i['purchaseRate']
+
+        for j in data['exchangeRate']:
+            for key, value in j.items():
+                if value == my_input2:
+                    result_summ = result_summ / j['saleRate']
+
+        return result_summ
+
+
+    res = konv(my_input1, my_input2, summ_for_sale)
+    print('%.2f' % res, my_input2)
+
+else:
+    def konv(cur1, cur2, summ):
+        result_summ = 0
+
+        if cur1 == 'UAH':
+            for i in data['exchangeRate']:
+                for k, v in i.items():
+                    if v == my_input2:
+                        result_summ = summ / i['saleRate']
+                        return result_summ
+        elif cur2 == 'UAH':
+            for i in data['exchangeRate']:
+                for k, v in i.items():
+                    if v == my_input1:
+                        result_summ = summ * i['purchaseRate']
+                        return result_summ
+
+        for i in data['exchangeRate']:
+            for k, v in i.items():
+                if v == my_input1:
+                    result_summ = summ * i['purchaseRate']
+
+        for j in data['exchangeRate']:
+            for key, value in j.items():
+                if value == my_input2:
+                    result_summ = result_summ / j['saleRate']
+
+        return result_summ
+
+
+    res = konv(my_input1, my_input2, summ_for_sale)
+    print('%.2f' % res, my_input2)
